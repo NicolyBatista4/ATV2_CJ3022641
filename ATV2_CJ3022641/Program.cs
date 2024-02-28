018b7c57f5745965b2b4dae81ff8cde0e74bc686
CJ3022641 <CJ3022641@LAB01-PC35>
2024-02-28 09:48:46 -0300

    Adicionar arquivos de projeto.

---
+++ ATV2_CJ3022641/Program.cs
@@ -0,0 +1,29 @@
+﻿
+using System.Security.Claims;
+using System.Threading.Channels;
+
+namespace ATV2_CJ3022641
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float salario;
+            // Leitura do salário
+            Console.WriteLine("Entre com o seu salário");
+            salario = float.Parse (Console.ReadLine() );
+
+            salario = salario + (25.0f/100.0f) * salario;
+
+            Console.ReadKey();
+
+               
+            
+              
+                
+                
+
+
+        }
+    }
+}
